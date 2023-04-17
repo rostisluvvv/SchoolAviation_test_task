64b051fd3ebeba255e9cf80b33e95fb6fd631c35
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-04-17 11:54:30 +0300

    small fix

--- educational_modules/edc_module/models.py
+++ educational_modules/edc_module/models.py
@@ -1,16 +1,11 @@
 from django.db import models
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
 
 
 class EdcModule(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=250, blank=True, null=True)
-    order_number = models.IntegerField(unique=True)
+    order_number = models.IntegerField(unique=True, null=True)
 
+    def __str__(self):
+        return self.name
 
-@receiver(pre_save, sender=EdcModule)
-def set_order_number(sender, instance, **kwargs):
-    if not instance.order_number:
-        last_module = EdcModule.objects.order_by('-order_number').first()
-        instance.order_number = last_module.order_number + 1 if last_module else 1
